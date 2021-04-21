3820143d101fccd59aa815937cd95c87050d5b12
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-21 16:34:06 -0300

    Add EnvironmentButton component

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,13 +1,65 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 
-import colors from '../../styles/colors'
 import { Header } from '../components/Header'
 
+import colors from '../../styles/colors'
+import fonts from '../../styles/fonts'
+import { EnvironmentButton } from '../components/EnvironmentButton'
+import api from '../services/api'
+
+type EnvironmentProps = {
+  key: string
+  title: string
+}
+
 export function PlantSelect() {
+  const [environments, setEnvironments] = useState<EnvironmentProps[]>()
+
+  useEffect(() => {
+    async function fetchEnvironment() {
+      const { data } = await api.get('plants_environments')
+      setEnvironments([
+        {
+          key: 'all',
+          title: 'Todos'
+        },
+        ...data
+      ])
+    }
+
+    fetchEnvironment()
+  }, [])
+
   return (
     <View style={ styles.container }>
-      <Header />
+      <View style={ styles.header }>
+        <Header />
+
+        <Text style={ styles.title }>
+          Em qual ambiente
+        </Text>
+        <Text style={ styles.subtitle }>
+          Você quer colocar sua planta?
+        </Text>
+      </View>
+
+      <View>
+        <FlatList
+          data={ environments }
+          renderItem={({ item }) => (
+            <EnvironmentButton
+              title={ item.title }
+              // active
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={ false }
+          contentContainerStyle={ styles.environmentList }
+          ListHeaderComponent={<View />}
+          ListHeaderComponentStyle={{ marginRight: 32 }}
+        />
+      </View>
     </View>
   )
 }
@@ -16,5 +68,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background
+  },
+
+  header: {
+    paddingHorizontal: 30
+  },
+
+  title: {
+    fontSize: 17,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 20,
+    marginTop: 15
+  },
+
+  subtitle: {
+    fontSize: 17,
+    fontFamily: fonts.text,
+    color: colors.heading,
+    lineHeight: 20,
+  },
+
+  environmentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginVertical: 32
   }
 })
