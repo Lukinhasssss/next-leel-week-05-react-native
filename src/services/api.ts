65009fb021b41103ec1baecf1b5f74a2aec6d234
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-24 19:57:13 -0300

    Update baseUrl for a future deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.59:3333'
+  baseURL: 'https://my-json-server.typicode.com/Lukinhasssss/next-leel-week-05-react-native'
 })
 
 export default api
