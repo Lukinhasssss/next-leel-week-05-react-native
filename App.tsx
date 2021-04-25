99fe862b881daccabaebb7b8d0a4666f09de2f35
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-25 12:30:37 -0300

    Add notifications, finish application

--- App.tsx
+++ App.tsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Jost_400Regular, Jost_600SemiBold, useFonts } from '@expo-google-fonts/jost'
 import AppLoading from 'expo-app-loading'
+import * as Notifications from 'expo-notifications'
 
 import Routes from './src/routes'
+import { PlantProps } from './src/libs/storage'
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
   })
+
+  useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantProps;
+        // console.log(data);
+      }
+    )
+    
+    return () => subscription.remove();
+
+    // async function notifications() {
+    //   // Cancelando todas as notificações
+    //   await Notifications.cancelAllScheduledNotificationsAsync()
+
+    //   // Mostrando todas as notifacações agendadas
+    //   const data = await Notifications.getAllScheduledNotificationsAsync()
+    //   console.log('######### NOTIFICAÇÕES AGENDADAS #########')
+    //   console.log(data)
+    // }
+    // notifications()
+  }, [])
 
   if (!fontsLoaded)
     return <AppLoading />
