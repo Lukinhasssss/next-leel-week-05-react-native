cb09bc608d17fd2247df5232505e3d42cf190da8
Lucas Monteiro <60019382+Lukinhasssss@users.noreply.github.com>
2021-04-28 20:23:10 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Lukinhasssss/next-leel-week-05-react-native'
+  baseURL: 'https://my-json-server.typicode.com/Lukinhasssss/next-level-week-05-react-native'
 })
 
 export default api
